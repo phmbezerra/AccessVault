27ddb45ab8fdf22921b53d488ac68f46c44db4fb
Paulo Henrique Bezerra <paulohenriquemelobezerra1@gmail.com>
2026-05-08 15:01:51 -0300

    Add auth router to main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from app.database.connection import engine
 from app.database.base import Base
 from app.models.user import User
-from app.routes import users
+from app.routes import users, auth
 
 app = FastAPI(title="AccessVault")
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 
 @app.get("/")
